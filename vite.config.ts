ecad061db847d414fe2e5a5321f04eb7b02bf5da
Wang Xu <wangxu627@gmail.com>
2025-12-18 22:02:58 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   // 读取构建配置
   const buildConfig = (() => {
+    console.log('读取构建配置 ::: ', process.env.BUILD_ENV)
+    
     try {
       const config = require('./build.config.json')
-      return config[process.env.BUILD_ENV || 'development'] || config.development
+      if (process.env.BUILD_ENV) {
+        return config[process.env.BUILD_ENV || 'development'] || config.development
+      } else {
+        return { baseUrl: '/' }
+      }
     } catch {
       return { baseUrl: '/' }
     }
